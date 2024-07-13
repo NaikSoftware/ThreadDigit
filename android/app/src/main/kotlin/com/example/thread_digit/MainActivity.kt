df44664416ed8704d5c39b0efcca9d62816842af
Nikolay Savchenko <naiksav4enko@gmail.com>
2024-07-13 17:10:48 +0300

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/thread_digit/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.thread_digit
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'ThreadDigit',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MainPage(title: 'ThreadDigit'),
+    );
+  }
+}
+
+class MainPage extends StatelessWidget {
+  const MainPage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  Widget build(BuildContext context) => Scaffold(
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          title: Text(title),
+        ),
+        body: Container(),
+      );
+}
